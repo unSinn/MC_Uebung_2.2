82a9ec26838894918de6bffac1bb90ec2e3bbe9a
fabian <fabian@fabi-ws>
2012-09-22 13:47:23 +0200

    Funktionierende Version.

--- MC/main.c
+++ MC/main.c
@@ -21,8 +21,6 @@
 char* reverse(char s[]);
 char* itoa(int i);
 
-#define MAXLAENGE 50
-
 /*
  * 
  */
@@ -34,18 +32,25 @@ int main(int argc, char *argv[]) {
     printf("===========================================\n");
 
 
-    int i = 76543;
+    /*
+     * Hier irgend einen Integer definieren.
+     */
+    int i = 123456;
+    
     printf("Input war: \"%d\"\n", i);
 
     char* output = itoa(i);
     printf("Das ganze als Sring: \"%s\"\n", output);
-
-    /*    reverse(s); */
+    
+    /*
+     * Speicher freigeben.
+     */
+    free(output);
 
     return (EXIT_SUCCESS);
 }
 
-/*char* reverse(char s[]) {
+char* reverse(char s[]) {
     int s_len = strlen(s);
     char rev[s_len];
     char c;
@@ -60,23 +65,59 @@ int main(int argc, char *argv[]) {
     rev[s_len] = '\0';
 
     return rev;
-}*/
+}
 
 /*
- * ASCII 48 – 57 – numbers '0'-'9'
+ * Gibt einen char-Pointer zurück, indem sich die der Integer i als char befindet.
+ * Am Ende befindet sich ein '\0'
+ * 
+ * ASCII 48 – 57 = numbers '0'-'9'
  */
 char* itoa(int i) {
     double l10 = log10(i);
+    
+    /*
+     * log_10(123) =  ~ 2.089
+     */
     int s_len = (int) l10 + 1;
+    
+    /*
+     * Speicher reservieren für die Länge des Strings + '\0'     
+     */
     char* output = (char*) malloc(sizeof (char) *
             s_len + 1);
 
-    char eis = 48;
-    output[0] = eis;
-    char zwoi = 49;
-    output[1] = zwoi;
+    int j = s_len - 1;
+    /*
+     * Wir identifizieren jeweils immer die letzte Ziffer und teilen i dann durch 10.
+     *  (Wir schieben i eins nach rechts)
+     * 
+     * Wenn wir bei i < 10 sind, wird i = 0.
+     *  (5/10 = 0 weil i ein ganzzahliger Datentyp ist)
+     */
+    while (i > 0) {
+        /* 
+         * i % 10 gibt jeweils die letzte Ziffer zurück.
+         *  (123 % 10 = 3)
+         * 
+         * UND
+         * 
+         *  (char c = 48 + 1 = '1')
+         * 
+         */
+        output[j] = 48 + (i % 10);
+        i = i / 10;
+        j--;
+    }
 
-    output[s_len + 1] = '\n';
+    output[s_len] = '\0';
+
+    /*
+     * Nun ist das ganze noch verkehrt, also drehen wir den String mit der Methode
+     * aus Übung 2.1 um.
+     */
+    reverse(output);
+
     return output;
 }
 
