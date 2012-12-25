2bb3508ea727650c63238b85aa97f768ceb7e560
fabian <fabian@Oyn>
2012-12-25 14:29:23 +0100

    up

--- MC/main.c
+++ MC/main.c
@@ -3,13 +3,18 @@
  * Authoren: Fabian Mächler, Erismann Marcel
  *
  * Übung 2.2: Integer to String
- * Schreiben Sie eine Funktion char* itoa(int i), welche die übergebene Integerzahl in
- * dezimaler Darstellung als Zeichenkette zurück gibt. Die dazu benötigte Zeichenkette soll
+ * Schreiben Sie eine Funktion char* itoa(int i), welche die übergebene 
+ * Integerzahl in
+ * dezimaler Darstellung als Zeichenkette zurück gibt. 
+ * Die dazu benötigte Zeichenkette soll
  * dynamisch alloziert werden.
- * Zur Berechnung der Anzahl Zeichen eines Wertes in dezimaler Darstellung können Sie den 10er
- * Logarithmus mit der Funktion log10(...) berechnen. Den Absolutwert einer Zahl erhalten Sie mit abs().
+ * Zur Berechnung der Anzahl Zeichen eines Wertes in 
+ * dezimaler Darstellung können Sie den 10er Log
+ * Logarithmus mit der Funktion log10(...) berechnen. 
  *
  * Created on September 18, 2012, 3:37 PM
+ * 
+ * Der code baut auf dem von Fabian Mächler auf.
  */
 
 #include <stdio.h>
@@ -17,8 +22,6 @@
 #include <string.h>
 #include <math.h>
 
-
-char* reverse(char s[]);
 char* itoa(int i);
 
 /*
@@ -27,44 +30,32 @@ char* itoa(int i);
 int main(int argc, char *argv[]) {
 
     printf("===========================================\n");
-    printf("Übung 2.2\n");
-    printf("Authoren: Fabian Mächler, Erismann Marcel\n");
+    printf("Uebung 2.2\n");
+    printf("Authoren: Fabian Maechler, Erismann Marcel\n");
     printf("===========================================\n");
 
 
     /*
      * Hier irgend einen Integer definieren.
      */
-    int i = 123456;
-    
-    printf("Input war: \"%d\"\n", i);
+    int i;
+
+    printf("Integer Zahl in String umwandeln:\n");
+    printf("Geben Sie eine Zahl ein: \n");
+    scanf("%d", &i); 
 
     char* output = itoa(i);
-    printf("Das ganze als Sring: \"%s\"\n", output);
-    
+    printf("Die Zahl als String: \"%s\"\n", output);
+
     /*
      * Speicher freigeben.
      */
-    free(output);
+    free(output); /* Gibt den Speicher wieder frei */
 
-    return (EXIT_SUCCESS);
-}
+    printf("===========================================\n");
+    printf("Ende der Uebung 2.2");
 
-char* reverse(char s[]) {
-    int s_len = strlen(s);
-    char rev[s_len];
-    char c;
-    int i;
-
-    for (i = 0; i < s_len; i++) {
-        c = s[i];
-        if (c != '\0') {
-            rev[s_len - 1 - i] = c;
-        }
-    }
-    rev[s_len] = '\0';
-
-    return rev;
+    return (0);
 }
 
 /*
@@ -74,52 +65,45 @@ char* reverse(char s[]) {
  * ASCII 48 – 57 = numbers '0'-'9'
  */
 char* itoa(int i) {
-    double l10 = log10(i);
-    
+    double lg = log10(i);
+
     /*
+     * zum Beispiel sei i: 123
      * log_10(123) =  ~ 2.089
      */
-    int s_len = (int) l10 + 1;
-    
+    int s_len = (int) lg + 1; /*s_len hat gleichviele Stellen wie dei eingegebende Zahl*/
+
     /*
      * Speicher reservieren für die Länge des Strings + '\0'     
      */
-    char* output = (char*) malloc(sizeof (char) *
-            s_len + 1);
+    char* output = (char*) malloc(sizeof (char) * s_len + 1);
 
-    int j = s_len - 1;
+    int j = s_len - 1; /* wir wollen das output-Array von der zweithintersten zum ersten Index auffüllen */
     /*
      * Wir identifizieren jeweils immer die letzte Ziffer und teilen i dann durch 10.
-     *  (Wir schieben i eins nach rechts)
-     * 
-     * Wenn wir bei i < 10 sind, wird i = 0.
-     *  (5/10 = 0 weil i ein ganzzahliger Datentyp ist)
+     *
      */
     while (i > 0) {
         /* 
          * i % 10 gibt jeweils die letzte Ziffer zurück.
          *  (123 % 10 = 3)
-         * 
          * UND
-         * 
+         * der int Wert 48 entspricht dem Ascii 0
          *  (char c = 48 + 1 = '1')
-         * 
+         *  48 +(123 %10)...48 +3= 51..51 in ASCIi ist 3..die 3 
+         * wir im im array beim index j der ist ja momentan der zweithinterste gespeichert
+         *
+         * 123/10 = 12 weil int und so
          */
-        output[j] = 48 + (i % 10);
-        i = i / 10;
+        
+        output[j] = 48 + (i % 10);  
+        i = i / 10; 
         j--;
     }
 
-    output[s_len] = '\0';
+    /*die letzte Speicherbox der char array wird mit dem \0 abgefüllt*/
+    output[s_len] = '\0'; 
 
-    /*
-     * Nun ist das ganze noch verkehrt, also drehen wir den String mit der Methode
-     * aus Übung 2.1 um.
-     */
-    reverse(output);
 
     return output;
 }
-
-
-
